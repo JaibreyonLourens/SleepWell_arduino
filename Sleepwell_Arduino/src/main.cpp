0f90db539ce77e5a63df4922a85da3901dd224ad
thorvald van swieten <4790482@student.fontys.nl>
2021-11-02 14:37:22 +0100

    Revert "schijven code ontvangen pulse"
    This reverts commit 2957f81fa0caea3833222a3d29e02cdf14542b12.

--- Sleepwell_Arduino/src/main.cpp
+++ Sleepwell_Arduino/src/main.cpp
@@ -1,74 +1,9 @@
 #include <Arduino.h>
 
-int rawValue;
-  
-  
-bool heartbeatDetected (int IRSensorPin, int delay)
-{
-  static int maxValue = 0;
-  static bool isPeak = false;
-    
-    
-  bool result = false;
-      
-  rawValue = analogRead (IRSensorPin);
-  rawValue *= (1000 / delay);
-  
-  
-  if (rawValue * 4L <maxValue) {maxValue = rawValue * 0.8; } 
-  if (rawValue> maxValue - (1000 / delay)) {
-    
-    if (rawValue> maxValue) {
-      maxValue = rawValue;
-    }
-    
-    if (isPeak == false) {
-      result = true;
-    }
-    isPeak = true;
-  } else if (rawValue <maxValue - (3000 / delay)) {
-    isPeak = false;
-   
-    maxValue -= (1000 / delay);
- }
-  return result;
+void setup() {
+  // put your setup code here, to run once:
 }
-  
-  
 
-int ledPin = 13;
-int analogPin = 0;
-  
-void setup ()
-{
-  
-  pinMode (ledPin, OUTPUT);
-    
- 
-  Serial.begin (9600);
-  Serial.println ("Heartbeat detection sample code.");
-}
-  
-const int delayMsec = 60; 
-  
-
-  
-void loop ()
-{
-  static int beatMsec = 0;
-  int heartRateBPM = 0;
-  if (heartbeatDetected (analogPin, delayMsec)) {
-    heartRateBPM = 60000 / beatMsec;
-    digitalWrite (ledPin, 1);
-  
-   
-    Serial.print ("Pulse detected:");
-    Serial.println (heartRateBPM);
-      
-    beatMsec = 0;
-  } else {
-    digitalWrite (ledPin, 0);
-  }
-  delay (delayMsec);
-  beatMsec += delayMsec;
+void loop() {
+  // put your main code here, to run repeatedly:
 }
